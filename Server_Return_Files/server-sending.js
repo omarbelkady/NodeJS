365a651be8ff61876b23cbdee031c9287b4013d1
Omar <omar.belkady@g.austincc.edu>
2020-09-11 17:51:59 +0100

    New files

--- Server_Return_Files/server-sending.js
+++ Server_Return_Files/server-sending.js
@@ -2,17 +2,17 @@ const http = require("http");
 const fs = require("fs");
 
 const server = http.createServer(function(req, res) {
-  let img = __dirname + "/pod.jpg";
+  let img = __dirname + "/favlang.jpg";
 
   fs.access(img, fs.constants.F_OK, err => {
     //check that we can access  the file
-    console.log(`${img} ${err ? "does not exist" : "exists"}`);
+    console.log(`${img} ${err ? "File does not exist" : "exists"}`);
   });
 
   fs.readFile(img, function(err, content) {
     if (err) {
       res.writeHead(404, { "Content-type": "text/html" });
-      res.end("<h1>No such image</h1>");
+      res.end("<h1>There is no such image 968 6342 56837</h1>");
     } else {
       //specify the content type in the response will be an image
       res.writeHead(200, { "Content-type": "image/jpg" });
@@ -20,6 +20,6 @@ const server = http.createServer(function(req, res) {
     }
   });
 });
-server.listen(1234, function() {
-  console.log("Server running on port 1234");
+server.listen(27678, function() {
+  console.log("Server running on port 27678 Nelan(C-Port) Fav Port");
 });
