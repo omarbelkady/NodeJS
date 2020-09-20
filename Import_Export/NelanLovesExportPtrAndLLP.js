d872681193fb62927dbd1ddbf74fde75af796608
Omar <omar.belkady@g.austincc.edu>
2020-09-20 03:37:52 +0100

    How To Use Imports And Exports In JS 968 746867 56837

---
+++ Import_Export/NelanLovesExportPtrAndLLP.js
@@ -0,0 +1,9 @@
+let myfunc1 = function whichAreaHeLovesToCodeIn(plang){
+	console.log(`Hello, 6342 56837 Go Code In C, Assembly, MC, ${plang}  because 968 5683 LLP`);
+}
+
+let myfunc2 = function favSchoolSubject(subj){
+	console.log(`Hello There, 2526 56837: ${subj}!`);
+}
+
+module.exports= {myfunc1,myfunc2}
---
+++ Import_Export/main.js
@@ -0,0 +1,7 @@
+//favSchoolSubject
+//whichAreaHeLovesToCodeIn
+const nelanLovesPtrs=require('./NelanLovesExportPtrAndLLP.js');
+let clover = "Compilers with C";
+let hisFavOS = "Pintos"
+nelanLovesPtrs.myfunc1(hisFavOS);
+nelanLovesPtrs.myfunc2(clover);
